f4efa00092aca3f500d10d774af37aa89d2bcaff
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-11-16 00:24:53 +0530

    map generator class added

---
+++ BrickBreakerGame/src/brickBreaker/MapGenerator.java
@@ -0,0 +1,13 @@
+package brickBreaker;
+
+public class MapGenerator {
+	
+	public int map[][];
+	public int brickWidth;
+	public int brickHeight;
+	
+	public MapGenerator(int row, int col) {
+		map = new int[row][col];
+	}
+
+}
