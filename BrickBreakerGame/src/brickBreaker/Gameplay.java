92c25f4549650eac314d3196fe5b4a7ffbe79bd9
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-11-16 12:16:41 +0530

    Game over message dsplay

--- BrickBreakerGame/src/brickBreaker/Gameplay.java
+++ BrickBreakerGame/src/brickBreaker/Gameplay.java
@@ -67,6 +67,16 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 		g.setColor(Color.orange);
 		g.fillOval(ballposX, ballposY, 20, 20);
 		
+		if(ballposY > 570) {
+			play = false;
+			ballXdir = 0;
+			ballYdir = 0;
+			g.setColor(Color.red);	
+			g.setFont(new Font("serif", Font.BOLD, 30));
+			g.drawString("Game Over! Your score:"+score,170,300);
+			g.setFont(new Font("serif", Font.BOLD, 20));
+			g.drawString("Press ENTER to restart!",240,350);
+		}
 		g.dispose();
 	}
 	
